4616d143a8b1131d9ab81f09801e6802032f45c4
Ryan <ryan@scalesec.com>
2018-12-03 11:15:08 -0800

    Added route table association

--- examples/terraform-ec2/network.tf
+++ examples/terraform-ec2/network.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "main" {
   vpc_id = "${aws_vpc.main.id}"
 
   tags {
@@ -13,12 +13,12 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
-resource "aws_route_table" "r" {
+resource "aws_route_table" "main" {
   vpc_id = "${aws_vpc.main.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.gw.id}"
+    gateway_id = "${aws_internet_gateway.main.id}"
   }
 
   tags {
@@ -33,4 +33,9 @@ resource "aws_subnet" "main" {
   tags {
     Name = "PublicTest"
   }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = "${aws_subnet.main.id}"
+  route_table_id = "${aws_route_table.main.id}"
 }
